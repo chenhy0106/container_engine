3987ea02d17e1f8c9a5b61faeab7e3e19c2cf694
Your Name <1002138131@qq.com>
2022-05-26 17:59:26 +0800

    添加cpu和mem资源控制。

--- engine.py
+++ engine.py
@@ -1,6 +1,8 @@
+from ast import parse
 import subprocess
 import os
 import sys
+import argparse
 
 class Define:
     def __init__(self) -> None:
@@ -49,24 +51,6 @@ def getIPaddr(Id):
 
     return addr_str
 
-def getOpt(net_ns):
-    cmd = ["unshare"]
-# unshare_cmd = ["unshare", "--uts", --net=" + D.NETNSFILE + self.net_ns + " --ipc --user --mount --pid --mount-proc --fork /bin/bash"
-        
-    cmd.append("--uts")
-    cmd.append("--ipc")
-    cmd.append("--user")
-    cmd.append("--mount")
-    cmd.append("--pid")
-    cmd.append("--map-root-user")
-    cmd.append("--root")
-    cmd.append("centos")
-    cmd.append("--mount-proc")
-    cmd.append("--net="+D.NETNSFILE+net_ns)
-    cmd.append("--fork")
-    cmd.append("/bin/bash")
-    return cmd
-
 
 
 class Container:
@@ -74,6 +58,7 @@ class Container:
     ip = ""
     Id = 0
     pid = 0
+    root_dir = ""
 
     @staticmethod
     def __create_bridge(bridgeName):
@@ -90,12 +75,42 @@ class Container:
             set_bridge_ip = ["ip addr add", D.BRIDGE_IP, "dev", bridgeName]
             activate_bridge_cmd = ["ip link set dev", bridgeName, "up"]
 
-            execCmd([create_bridge_cmd, set_bridge_ip, activate_bridge_cmd])
+            iptables_i = ["iptables", "-A", "FORWARD", "-i", bridgeName, "-j", "ACCEPT"]
+            iptables_o = ["iptables", "-A",
+                                    "FORWARD", "-o", bridgeName, "-j", "ACCEPT"]
+            # 把物理eth也放入网桥，从而veth-pair可以ping通外网
+            iptables_nat = ["iptables", "-t", "nat", "-A", "POSTROUTING",
+                                  "-s", "10.0.1.0/24", "-o", "eth0", "-j", "MASQUERADE"]
 
-    def __init(self):
+            execCmd([create_bridge_cmd, set_bridge_ip, activate_bridge_cmd, iptables_i, iptables_o, iptables_nat])
+
+    def __getStartCmd(self):
+        cgroup_name = "Container_" + str(self.Id)
+        cmd = ["cgexec"]
+        cmd.append("-g")
+        cmd.append("cpu,cpuset,memory:"+cgroup_name)
+        cmd.append("unshare")
+        
+        cmd.append("--uts")
+        cmd.append("--ipc")
+        cmd.append("--user")
+        cmd.append("--mount")
+        cmd.append("--pid")
+        cmd.append("--root")
+        cmd.append(self.root_dir)
+        cmd.append("--mount-proc")
+        cmd.append("--net="+D.NETNSFILE+self.net_ns)
+        cmd.append("--fork")
+        cmd.append("/bin/bash")
+
+        print(cmd)
+        return cmd
+
+    def __init(self, root_dir):
         self.Id = getID()
         self.ip = getIPaddr(self.Id)
         self.net_ns = "Container_" + str(self.Id)
+        self.root_dir = root_dir
 
         res = os.popen("ip netns ls").readlines()
         new_netns = True
@@ -125,10 +140,12 @@ class Container:
         attach_veth1_on_bridge = ["ip link set dev", veth1, "master", bridgeName]
         activate_veth1 = ["ip link set dev", veth1, "up"]
 
-        execCmd([create_veth, attach_veth0_on_netns, set_veth0_ip, activate_veth0, activate_veth0_loop, \
-            set_veth1_ip, attach_veth1_on_bridge, activate_veth1])
+        set_gateway = ["ip", "netns", "exec", self.net_ns, "ip", "route", "add", "default", "via", D.BRIDGE_IP[:-3]]
 
-    def __setpid(self, pid):
+        execCmd([create_veth, attach_veth0_on_netns, set_veth0_ip, activate_veth0, activate_veth0_loop, \
+            set_veth1_ip, attach_veth1_on_bridge, activate_veth1, set_gateway])
+
+    def __configpid(self, pid):
         self.pid = pid
 
     def __configUser(self, user_opt):
@@ -141,28 +158,88 @@ class Container:
 
         execCmd([set_gid, set_uid])
 
+    def __configMemory(self, mem_opt):
+        # if mem_opt < 4096:
+            # memory limit must be larger than 4096
+            # mem_opt = 4096
+
+        cgroup_name = "Container_" + str(self.Id)
+        cgcreate = ["cgcreate", "-g", "memory:/"+cgroup_name]
+        cgset = ["cgset", "-r", "memory.limit_in_bytes="+str(mem_opt), cgroup_name]
+        cgclassify = ["cgclassify", "-g", "memory:"+cgroup_name, cgroup_name]
+
+        execCmd([cgcreate, cgset])
+
+    def __configCPU(self, cpu_opt):
+        cpuset = cpu_opt[0]
+        cpushare = str(cpu_opt[1])
+        cpuset_str = "0"
+        if cpuset > 1:
+            cpuset_str = cpuset_str + "-" + str(cpuset-1)
+        
+        cgroup_name = "Container_" + str(self.Id)
+        cgcreate_cpuset = ["cgcreate", "-g", "cpuset:/"+cgroup_name]
+        cgset_cpuset_cpus = ["cgset", "-r", "cpuset.cpus="+cpuset_str, cgroup_name]
+        cgset_cpuset_mems = ["cgset", "-r", "cpuset.mems=0", cgroup_name]
+        cgclassify_cpuset = ["cgclassify", "-g", "cpuset:"+cgroup_name, cgroup_name]
+        cgcreate_cpu = ["cgcreate", "-g", "cpu:/"+cgroup_name]
+        cgset_cpu_cpushare = ["cgset", "-r", "cpu.shares="+cpushare, cgroup_name]
+        cgclassify_cpu = ["cgclassify", "-g", "cpu:"+cgroup_name, cgroup_name]
+
+        execCmd([cgcreate_cpuset, cgset_cpuset_cpus, cgset_cpuset_mems, \
+            cgcreate_cpu, cgset_cpu_cpushare])
+
+    def __chowner(self):
+            chown_to_root = ["chown", "-R", "root:root", self.root_dir]
+            execCmd([chown_to_root])
+
     def __destory(self):
+        # destory net
         veth1 = self.net_ns + "_1"
         destory_veth1 = ["ip link delete", veth1]
-
         # destory_netns = ["ip netns delete"]
-        execCmd([destory_veth1])
 
-    def run(self, net_opt, user_opt):
-        self.__init()
+        #destory cgroup
+        cgroup_name = "Container_" + str(self.Id)
+        cgdelete_mem = ["cgdelete", "memory:"+cgroup_name]
+        cgdelete_cpu = ["cgdelete", "cpu:"+cgroup_name]
+        cgdelete_cpuset = ["cgdelete", "cpuset:"+cgroup_name]
 
-        sub = subprocess.Popen(getOpt(self.net_ns))
+        execCmd([destory_veth1, cgdelete_mem, cgdelete_cpu, cgdelete_cpuset])
+
+    def run(self, net_opt, root_dir, user_opt, mem_opt, cpu_opt):
+
+        self.__init(root_dir)
+        self.__configMemory(mem_opt)
+        self.__configCPU(cpu_opt)
+
+        sub = subprocess.Popen(self.__getStartCmd())
         print("container ID : " + str(self.Id) + ", IP : " + str(self.ip))
         
-        self.__setpid(sub.pid)
+        self.__configpid(sub.pid)
         self.__configNetwork(net_opt)
-        # self.__configUser(user_opt)
+        self.__configUser(user_opt)
+        self.__chowner()
         
         sub.wait()
 
         self.__destory()
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r", "--root", type=str, dest="rootDir", default="")
+    parser.add_argument("-n", "--net", type=str, dest="net_opt", default="Default")
+    parser.add_argument("-m", "--memory", type=str, dest="mem_opt", default="1M")
+    parser.add_argument("-u", "--uid", type=int, dest="uid", default=0)
+    parser.add_argument("-g", "--gid", type=int, dest="gid", default=0)
+    parser.add_argument("--cpus", type=int, dest="cpus", default=1)
+    parser.add_argument("--cpu-share", type=int, dest="cpushare", default=256)
+    args = parser.parse_args()
+
     container = Container()
 
-    container.run(net_opt="Default", user_opt=(1000, 1000))
+    container.run(  net_opt=args.net_opt, \
+                    root_dir=args.rootDir, \
+                    user_opt=(args.uid, args.gid), \
+                    mem_opt=args.mem_opt, \
+                    cpu_opt=(args.cpus, args.cpushare))
